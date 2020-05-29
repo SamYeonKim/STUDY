934df10bee337c034fcaba04216f13cf321e5f5e
SamYeonKim <scrc8603@ncsoft.com>
2020-05-29 18:25:14 +0900

    add SUB

---
+++ Unity/SimpleBrowser/UnityClient/Assets/SimpleWebBrowser/Demo/StressTest.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleWebBrowser;
+
+public class StressTest : MonoBehaviour
+{
+    public RectTransform browserTf;
+    public int width = 1024;
+    public int height = 768;
+    public int count = 1;
+
+    public int deltaX = 10;
+    public int deltaY = 10;
+
+    string[] urlVariation = {
+        "https://google.com",
+         "https://naver.com",
+          "https://ncsoft.com",
+           "https://unity.com",
+            "https://bitbucket.org/vitaly_chashin/simpleunitybrowser/src/default/UnityClient/Assets/SimpleWebBrowser/PluginServer",
+            "http://danawa.com/",
+            "https://www.daum.net/",
+            "https://www.nate.com/",
+            "https://leetcode.com/",
+            "https://kr.louisvuitton.com/kor-kr/homepage",
+             };
+
+    private string strWidth;
+    private string strHeight;
+    private string strCount;
+
+    private List<WebBrowser2D> createdBrowser = new List<WebBrowser2D>();
+    
+    private void Start() {
+        strWidth = width.ToString();    
+        strHeight = height.ToString();
+        strCount = count.ToString();
+    }
+    private void OnGUI() {
+
+        GUILayout.BeginHorizontal();
+
+        strWidth = GUILayout.TextField(strWidth, GUILayout.Width(Screen.width * 0.2f));
+        strHeight = GUILayout.TextField(strHeight, GUILayout.Width(Screen.width * 0.2f));
+        strCount = GUILayout.TextField(strCount, GUILayout.Width(Screen.width * 0.2f));
+
+        if ( GUILayout.Button("Start Test") ) 
+        {
+            width = int.Parse(strWidth);
+            height = int.Parse(strHeight);
+            count = int.Parse(strCount);
+
+            for (int i = 0; i < count; i++)
+            {
+                var newBrowserGo = GameObject.Instantiate(browserTf.gameObject, Vector3.zero, Quaternion.identity, browserTf.parent );
+                newBrowserGo.SetActive(false);
+
+                RectTransform transform = newBrowserGo.GetComponent<RectTransform>();
+                transform.Translate(deltaX * i, deltaY* i , 0);
+                WebBrowser2D browser = newBrowserGo.GetComponent<WebBrowser2D>();
+                browser.Width = width;
+                browser.Height = height;
+
+                browser.InitialURL = urlVariation[i%urlVariation.Length];
+
+                newBrowserGo.SetActive(true);
+
+                createdBrowser.Add(browser);
+            }
+        }
+
+        if ( GUILayout.Button("CLear") )
+        {
+            foreach (var item in createdBrowser)
+            {
+                item.gameObject.SetActive(false);
+                GameObject.Destroy(item.gameObject);
+            }
+
+            createdBrowser.Clear();
+        }
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ Unity/SimpleBrowser/UnityClient/Assets/SimpleWebBrowser/Editor/BrowserPostBuild.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.IO;
+using System.Linq;
+
+public class BrowserPostBuild
+{
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostprocessBuild(BuildTarget target, string path)
+    {
+        string dstPath = Path.GetDirectoryName(path);
+        
+        Debug.Log("Post Build:"+ dstPath);       
+                
+        switch (target)
+        {
+            case BuildTarget.StandaloneWindows64:
+            case BuildTarget.StandaloneWindows:
+            {                   
+                string dstPluginPath = Path.Combine(Path.Combine(dstPath, Path.GetFileNameWithoutExtension(path) + "_Data"), "Plugins");
+                string pluginPath = GetTargetPluginPath(target);
+                string fullPath = Application.dataPath.Replace("Assets", pluginPath);
+                
+                string[] files=Directory.GetFiles(fullPath);
+                foreach (var file in files)
+                {
+                    if (!file.Contains("meta"))
+                    {
+                        if(!File.Exists(Path.Combine(dstPluginPath, Path.GetFileName(file))))
+                            FileUtil.CopyFileOrDirectory(file, Path.Combine(dstPluginPath, Path.GetFileName(file)));
+                    }
+                }
+
+                string dstLocalePath = Path.Combine(dstPluginPath, "locales");
+                Directory.CreateDirectory(dstLocalePath);
+                files = Directory.GetFiles(Path.Combine(fullPath, "locales"));
+                foreach (var file in files)
+                {
+                    if (!file.Contains("meta"))
+                    {
+                        if (!File.Exists(Path.Combine(dstLocalePath, Path.GetFileName(file))))
+                            FileUtil.CopyFileOrDirectory(file, Path.Combine(dstLocalePath,Path.GetFileName(file)));
+                    }
+                }
+
+                break;
+            }            
+            default:
+                Debug.LogError("Web browser is not supported on this platform!");
+                break;
+        }
+    }
+
+    private static string GetTargetPluginPath(BuildTarget target)
+    {   
+        string pluginPath = "";
+        var assets = AssetDatabase.FindAssets("Xilium.CefGlue");
+        foreach (var item in assets)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(item);
+            string dirFullPath = Path.GetDirectoryName(path);
+            string dirName = Path.GetDirectoryName(path);
+
+            PluginImporter importer = (PluginImporter)AssetImporter.GetAtPath(path);
+
+            if ( importer.GetCompatibleWithPlatform(target) )
+            {
+                pluginPath = dirFullPath;
+                break;
+            }
+        }
+
+        return pluginPath;
+    }
+}
